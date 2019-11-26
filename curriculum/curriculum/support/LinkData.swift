6507e6c11b7f9b8051da70447667210dfc32bc90
Timofey <timaluk@icloud.com>
2019-11-26 22:15:00 +0300

    parsing

--- curriculum/curriculum/support/LinkData.swift
+++ curriculum/curriculum/support/LinkData.swift
@@ -5,7 +5,6 @@
 //  Created by Тимофей Лукашевич on 11/23/19.
 //  Copyright © 2019 IvanLyuhtikov. All rights reserved.
 //
-
 //import Foundation
 
 
@@ -19,14 +18,14 @@ var links = [
     "Т-793"  :  "https://kbp.by/rasp/timetable/view_beta_kbp/?cat=group&id=98",
     "Т-794"  :  "https://kbp.by/rasp/timetable/view_beta_kbp/?cat=group&id=99",
     "Т-795"  :  "https://kbp.by/rasp/timetable/view_beta_kbp/?cat=group&id=89",
-//    "Т-891"  :  "https://kbp.by/rasp/timetable/view_beta_kbp/?cat=group&id=115",
+    "Т-891"  :  "https://kbp.by/rasp/timetable/view_beta_kbp/?cat=group&id=115",
     "Т-892"  :  "https://kbp.by/rasp/timetable/view_beta_kbp/?cat=group&id=120",
-//    "Т-893"  :  "https://kbp.by/rasp/timetable/view_beta_kbp/?cat=group&id=117",
-//    "Т-991"  :  "https://kbp.by/rasp/timetable/view_beta_kbp/?cat=group&id=75",
-//    "Т-992"  :  "https://kbp.by/rasp/timetable/view_beta_kbp/?cat=group&id=76",
-//    "Т-993"  :  "https://kbp.by/rasp/timetable/view_beta_kbp/?cat=group&id=77",
-//    "Т-994"  :  "https://kbp.by/rasp/timetable/view_beta_kbp/?cat=group&id=78",
-//    "Т-995"  :  "https://kbp.by/rasp/timetable/view_beta_kbp/?cat=group&id=79",
+    "Т-893"  :  "https://kbp.by/rasp/timetable/view_beta_kbp/?cat=group&id=117",
+    "Т-991"  :  "https://kbp.by/rasp/timetable/view_beta_kbp/?cat=group&id=75",
+    "Т-992"  :  "https://kbp.by/rasp/timetable/view_beta_kbp/?cat=group&id=76",
+    "Т-993"  :  "https://kbp.by/rasp/timetable/view_beta_kbp/?cat=group&id=77",
+    "Т-994"  :  "https://kbp.by/rasp/timetable/view_beta_kbp/?cat=group&id=78",
+    "Т-995"  :  "https://kbp.by/rasp/timetable/view_beta_kbp/?cat=group&id=79",
     
     
     
